da150b7bf6f6b71c106bbb551df91120b61e855b
Rohan Grover <rohan7grover@gmail.com>
2023-10-10 22:47:04 +0530

    create and update user complete with authorization

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,37 @@
 const router = require("express").Router();
+const User = require("../models/user");
+const CryptoJS = require("crypto-js");
 
-router.get("/usertest", (req, res) => {
-    res.send("user test is successful");
+const {
+    verifyTokenAndAuthorization
+} = require("./verifyToken");
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        if (req.body.password) {
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.PASS_SEC
+            ).toString();
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json("User not found");
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-router.post("/userposttest", (req, res) => {
-    const username = req.body.username;
-    console.log(username);
-});
-
-module.exports = router
+module.exports = router;
