8d9b2dd485e46a686be335024c3a9dd8ef466873
Rohan Grover <rohan7grover@gmail.com>
2023-10-11 03:19:56 +0530

    added routes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -11,13 +15,16 @@ mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connection Successful"))
     .catch((err) => {
         console.error(err);
-});
+    });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend server is running");
 });
-
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,8 @@ const User = require("../models/user");
 const CryptoJS = require("crypto-js");
 
 const {
-    verifyTokenAndAuthorization
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
 } = require("./verifyToken");
 
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -31,6 +32,37 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query
+            ? await User.find().sort({ _id: -1 }).limit(5)
+            : await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
